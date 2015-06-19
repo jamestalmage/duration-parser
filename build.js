7bda82085685ab9bc0e38a92e993721f1baa55ec
James Talmage <james@talmage.io>
2015-06-19 13:49:48 -0400

    update build script

--- build.js
+++ build.js
@@ -1,8 +1,13 @@
 var fs = require('fs');
 var peg = require('pegjs');
+var pkg = require('./package.json');
 
 var grammar = fs.readFileSync(__dirname + '/grammar.pegjs', 'utf-8');
 
 var parserSource = peg.buildParser(grammar, {output:'source'});
 
-fs.writeFileSync(__dirname + '/index.js', 'module.exports = ' + parserSource);
+fs.writeFileSync(__dirname + '/index.js',
+  '// duration-parser ' + pkg.version + '\n' +
+  '//' + pkg.homepage + '\n' +
+  'module.exports = ' + parserSource
+);
